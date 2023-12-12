a399445e5aa0db1f87913562669e84dd2f9f3655
HUDACE <dtomibite@gmail.com>
2023-12-12 12:54:08 +0100

    tokencheck és pár crud művelet

--- modules/database.js
+++ modules/database.js
@@ -11,3 +11,67 @@ var pool  = mysql.createPool({
   database        : process.env.DBNAME,
   timezone: 'UTC'
 });
+
+// minden rekord lekérdezése
+router.get('/:table', (req, res) => {
+  let table = req.params.table;
+  pool.query(`SELECT * FROM ${table}`, (err, results) => {
+      sendResults(table, err, results, req, res, 'sent from');
+  });
+});
+
+// minden rekord lekérdezése
+router.get('/:table', (req, res) => {
+  let table = req.params.table;
+  pool.query(`SELECT * FROM ${table}`, (err, results) => {
+      sendResults(table, err, results, req, res, 'sent from');
+  });
+});
+
+// id alapján való lekérdezés
+router.get('/:table/:id', (req, res) => {
+  let table = req.params.table;
+  let id = req.params.id;
+  pool.query(`SELECT * FROM ${table} WHERE ID=${id}`, (err, results) => {
+      sendResults(table, err, results, req, res, 'sent from');
+  });
+});
+// mező alapján lekérés
+router.get('/:table/:field/:op/:value', tokencheck(), (req, res)=>{
+  let table = req.params.table;
+  let field = req.params.field;
+  let value = req.params.value;
+  let op = getOperator(req.params.op);
+
+  if (op == ' like '){
+      value = `%${value}%`;
+  }
+  pool.query(`SELECT * FROM ${table} WHERE ${field}${op}'${value}'`, (err, results)=>{
+      sendResults(table, err, results, req, res, 'sent from');
+  });
+});
+// update
+router.patch('/:table/:field/:op/:value', tokencheck(), (req, res) => {
+  let table = req.params.table;
+  let field = req.params.field;
+  let value = req.params.value;
+  let op = getOperator(req.params.op);
+
+  if (op == ' like '){
+      value = `%${value}%`;
+  }
+  let values = Object.values(req.body);
+  let fields = Object.keys(req.body);
+
+  let sql = '';
+  for(i=0; i< values.length; i++){
+      sql += fields[i] + `='` + values[i] + `'`;
+      if (i< values.length-1) {
+      sql += ',';
+      } 
+  }
+  pool.query(`UPDATE ${table} SET ${sql} WHERE ${field}${op}'${value}'`, (err, results)=>{
+      sendResults(table, err, results, req, res, 'updated in');
+  });
+});
+
--- modules/functions.js
+++ modules/functions.js
@@ -28,7 +28,25 @@ function getOperator(op){
     }
     return op;
 }
-
+function tokencheck(){
+    return (req, res, next) =>{
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        if (token == null){
+            console.log(notice(req.socket.remoteAddress) + ' >> ' + error('Tokencheck error!'));
+            return res.status(401).json({ message: "Illetéktelen hozzáférés!"});
+        }
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err){
+                console.log(notice(req.socket.remoteAddress) + ' >> ' + error('Tokencheck error!'));
+                return res.status(403).json({ message: "Hibás token!"});
+            }
+            req.user = user;
+            console.log(req.user)
+            next();
+        });
+    }
+}
 
 module.exports = { 
     sendResults, 
